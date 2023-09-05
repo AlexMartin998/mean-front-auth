31e314ea098ffa4b83efd9bbf1c8bb2495c34929
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-05 08:07:39 -0500

    [ADD]: connect form builder with html

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginPageComponent {
+  // // Inject by Attribute
+  // nueva forma de Inyectar en Angular, ya NO por constructor. Con esto incluso de puede Inyectar Services en Functiones de TS
+  private fb = inject(FormBuilder);
 
+  // // reactive form
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    if (this.myForm.invalid) return this.myForm.markAllAsTouched();
+
+    console.log(this.myForm.value);
+  }
 }
