23af8cc74ba58803e37bf04358e30c2e4b1657b3
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-05 09:19:02 -0500

    [ADD]: LoginResponse Interface

---
+++ src/app/auth/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface User {
+  name: string;
+  email: string;
+  isDeleted: boolean;
+  roles: string[];
+  id: string;
+}
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { AuthStatus, LoginResponse } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  // // Signal: _ no significa nada mas q en JS q es private, pero en TS ya tiene private
+  private _currentUser = signal<LoginResponse['user'] | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  // dar acceso al mundo exterior con ComputedSignals (signals de SOLO LECTURA) para q NADIE fuera del service pueda cambiar el value de los Signals
+  public currentUser = computed(() => this._currentUser);
+  public authStatus = computed(() => this._authStatus);
+
+  // // methods
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
