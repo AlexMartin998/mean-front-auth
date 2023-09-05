264c3d52e078590706c173108fd13e94b62562ca
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-05 10:31:03 -0500

    [ADD]: handle login error and display it with sweetalert

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,16 +13,29 @@ export class LoginPageComponent {
   // // Inject by Attribute
   // nueva forma de Inyectar en Angular, ya NO por constructor. Con esto incluso de puede Inyectar Services en Functiones de TS
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   // // reactive form
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['alex3@demo.com', [Validators.required, Validators.email]],
+    password: ['123qWe123', [Validators.required, Validators.minLength(6)]],
   });
 
   login() {
     if (this.myForm.invalid) return this.myForm.markAllAsTouched();
 
-    console.log(this.myForm.value);
+    const { email, password } = this.myForm.value;
+
+    // // dispara la req el .subscribe()
+    this.authService.login(email, password).subscribe({
+      next: () => console.log('todo ok'),
+      error: (errorMessage: string) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: errorMessage,
+        });
+      },
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
-import { AuthStatus, LoginResponse } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +13,7 @@ export class AuthService {
   private http = inject(HttpClient);
 
   // // Signal: _ no significa nada mas q en JS q es private, pero en TS ya tiene private
-  private _currentUser = signal<LoginResponse['user'] | null>(null);
+  private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
   // dar acceso al mundo exterior con ComputedSignals (signals de SOLO LECTURA) para q NADIE fuera del service pueda cambiar el value de los Signals
@@ -22,6 +22,23 @@ export class AuthService {
 
   // // methods
   login(email: string, password: string): Observable<boolean> {
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>(url, body).pipe(
+      // // si todo salio bien
+      tap(({ user, token }) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+
+        localStorage.setItem('token', token);
+      }),
+      map(() => true),
+
+      // // errors
+      // catchError no nos da flexibilidad, deberia retornar el boolean, pero con el throwError puedo hacer mas cosas  <-- ver impl en el .subscribe()
+      // atrapo y envio el message error del back
+      catchError((error) => throwError(() => error.error.message))
+    );
   }
 }
